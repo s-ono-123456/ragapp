b8e406cf98da34570599a6377f2bfb00f575268c
s-ono <you@example.com>
2025-04-19 07:51:19 +0900

    ページ追加

---
+++ pages/md_viewer.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import glob
+import os
+
+st.title("設計書表示アプリ")
+
+# sampleディレクトリのmdファイル一覧取得
+md_files = glob.glob("sample/*.md")
+md_file_names = [os.path.basename(f) for f in md_files]
+
+st.sidebar.title("sampleディレクトリのmdファイル")
+selected_md = st.sidebar.selectbox("表示するmdファイルを選択してください", md_file_names)
+
+if selected_md:
+    with open(os.path.join("sample", selected_md), "r", encoding="utf-8") as f:
+        md_content = f.read()
+    st.markdown(md_content)
